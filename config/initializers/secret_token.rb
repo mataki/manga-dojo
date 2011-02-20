297e3336d2f3c8767dc568f7b4ecf10f08124996
mat_aki <matsumura.aki@gmail.com>
2011-02-21 01:58:09 +0900

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MangaDojo::Application.config.secret_token = '5f2d924a52c780187adf10b5e9f1d2ed74ce50ce57eacc223685622f6d8842d86348133c7db9a0534d54e818089fbdf26ac664208c58e0716995004de13b8ff8'
