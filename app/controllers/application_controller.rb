9d359c8efaa8335e58d5ee7776fb892ea5fb2e2d
mat_aki <matsumura.aki@gmail.com>
2011-04-04 13:28:07 +0900

    Remove force Authorize

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,12 +9,14 @@ class ApplicationController < ActionController::Base
 
   private
   def filter_setup_rest_graph
-    rest_graph_setup(:auto_authorize => true, :write_cookies => true)
+    rest_graph_setup(:write_cookies => true)
   end
 
   def set_current_user
-    @current_user = User.find_or_create_by_rest_graph!(rest_graph)
-    @current_user.update_oauth_token(rest_graph.access_token)
+    if rest_graph.authorized?
+      @current_user = User.find_or_create_by_rest_graph!(rest_graph)
+      @current_user.update_oauth_token(rest_graph.access_token)
+    end
   end
 
   def current_user
