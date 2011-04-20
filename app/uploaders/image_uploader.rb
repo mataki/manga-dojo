e90aa26077b7a190b5d51019f29da05946c92968
Akihiro Matsumura <matsumura.aki@gmail.com>
2011-04-20 20:10:02 +0900

    Add link to edit manga

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -16,6 +16,10 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  def cache_dir
+    "#{Rails.root}/tmp/uploads"
+  end
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
